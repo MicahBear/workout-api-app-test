2f66c1e1d333d5bc07747fef1503a2d67327f1ff
MicahBear <micah.b.elm@gmail.com>
2022-09-15 15:08:35 -0700

    update to context

--- frontend/src/hooks/useWorkoutsContext.js
+++ frontend/src/hooks/useWorkoutsContext.js
@@ -5,5 +5,9 @@ import { useContext } from "react";
 export const useWorkoutsContext = () => {
     const context = useContext(WorkoutsContext)
 
+    if (!context) {
+        throw Error(' useWorkoutsContext must be used inside an WorkoutsContextProvider')
+    }
+
     return context
 }
